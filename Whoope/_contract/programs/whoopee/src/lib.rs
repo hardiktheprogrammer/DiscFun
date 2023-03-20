8020de6e819dcb86e79bf9066bc3463b28511246
patel7vishal <pv251023@gmail.com>
2023-03-20 22:16:29 +0530

    update lib.rs

--- Whoope/_contract/programs/whoopee/src/lib.rs
+++ Whoope/_contract/programs/whoopee/src/lib.rs
@@ -14,10 +14,4 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod whoopee {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
